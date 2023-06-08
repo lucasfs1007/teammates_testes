6d73d352044ea9db6a9324534339cc035d606545
Lucas Felipe Soares <88278278+lucasfs1007@users.noreply.github.com>
2023-06-08 10:59:36 -0300

    adiciona minhas modificações em cima do teste original

--- .github/Lucas - Testes/instant.java
+++ .github/Lucas - Testes/instant.java
@@ -1,17 +1,22 @@
 public static String formatInstant(Instant instant, String timeZone, String pattern) {
         if (instant == null || timeZone == null || pattern == null) {
-        return "";
-        }
-        ZonedDateTime zonedDateTime = instant.atZone(ZoneId.of(timeZone));
-        String processedPattern = pattern;
-        if (zonedDateTime.getHour() == 12 && zonedDateTime.getMinute() == 0) {
-        processedPattern = pattern.replace("a", "'NOON'");
-        }
-        DateTimeFormatter formatter = DateTimeFormatter.ofPattern(processedPattern);
-        return zonedDateTime.format(formatter);
+            // tratamos agora como uma exceção e não uma string vazia para indicar o erro
+        throw new IllegalArgumentException("Instant, timeZone, and pattern cannot be null");
         }
 
-/**
- * Gets an Instant which is adjusted for midnight time (23:59 and 00:00) at the specified time zone.
- * The direction of adjustment (23:59 to 00:00 or vice versa) is determined by {@code isForward} parameter.
- */
+        try {
+        ZonedDateTime zonedDateTime = instant.atZone(ZoneId.of(timeZone));
+        String processedPattern = pattern;
+        //simplifica a comparação para agora olharmos o horário de 12:00 independente dos minutos
+        if (zonedDateTime.toLocalTime().equals(LocalTime.NOON)) {
+        processedPattern = pattern.replace("a", "'NOON'");
+        }
+
+        DateTimeFormatter formatter = DateTimeFormatter.ofPattern(processedPattern);
+        return zonedDateTime.format(formatter);
+            //criamos esse catch apenas para tratar eventuais erros que possam vir a acontecer
+        } catch (DateTimeException e) {
+        e.printStackTrace();
+        return ""; //paralização do método
+        }
+        }
